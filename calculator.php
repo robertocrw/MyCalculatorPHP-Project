eae32aacab077b94f8f3f001c575d68ab0714c5b
Roberto Pinto <robertowashington.bailao@gmail.com>
2015-01-01 12:55:58 +0000

    adding button

--- calculator.php
+++ calculator.php
@@ -5,13 +5,34 @@
 </head>
 <body>
 	<form method="post" action="unica-entrada.html" name="formu">
+		<!--Entrada e Submite-->
 		<input type="text" name="entrada" readonly="readonly" />
-		<input type="submit" value="Testa"  />
+		
+		<table>
+             <tr>
+             	<td><input id="botao" type="button" value="C"></td>
+             	<td><input id="botao" type="button" value="√"></td>
+             	<td><input id="botao" type="button" value="%"></td>
+             	<td><input id="botao" type="button" value="/"></td>
+             </tr>
+		</table>
 	</form> 
 	<?php 
-    for ($i=0; $i<=9; $i++) {
+    for ($i=1; $i<=9; $i++) 
+    {
     	echo "<input type='button' value='$i'>";
+    	if ($i%3 ==0) {
+    		echo "<br/>";
+    	}
     }
 	?>
+	    <table>
+             <tr>
+             	<td><input id="botao" type="button" value="0"></td>
+             	<td><input id="botao" type="button" value="."></td>
+             	<td><input id="botao" type="button" value="="></td>
+             	<td><input id="botao" type="button" value="+"></td>
+             </tr>
+		</table>
 </body>
 </html>
