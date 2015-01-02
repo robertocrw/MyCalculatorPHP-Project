031de239493eeeaa0267902e53ae7c5c3ab37503
Roberto Pinto <robertowashington.bailao@gmail.com>
2015-01-02 14:25:08 +0000

    adding css

--- calculator.php
+++ calculator.php
@@ -2,37 +2,49 @@
 <html lang="pt-br">
 <head>
 	<meta charset="utf-8">
+    <style>
+      
+         #entrada { width: 197px; height: 45px; margin: 2px; margin-left: 3px;}
+         #numeros {margin: 2px; margin-left: 3px; height: 45px; width: 45px; background: green; color: white; }
+         #botao {margin: 1px; margin-left: 0px; height: 45px; width: 45px; background: green; color: white; }
+      
+    </style>
 </head>
+
 <body>
+<div>    
 	<form method="post" action="unica-entrada.html" name="formu">
 		<!--Entrada e Submite-->
-		<input type="text" name="entrada" readonly="readonly" />
+		<input id="entrada" type="text" name="entrada" readonly="readonly" />
 		
 		<table>
              <tr>
-             	<td><input id="botao" type="button" value="C"></td>
-             	<td><input id="botao" type="button" value="√"></td>
-             	<td><input id="botao" type="button" value="%"></td>
-             	<td><input id="botao" type="button" value="/"></td>
+             	 <td><input id="botao" type="button" value="C" onClick="entrada.value=''"></td>
+             	 <td><input id="botao" type="button" value="√" onClick="entrada.value+='√'"></td>
+             	 <td><input id="botao" type="button" value="%" onClick="entrada.value+='%'"></td>
+             	 <td><input id="botao" type="button" value="/" onClick="entrada.value+='/'"></td>
              </tr>
 		</table>
-	</form> 
+	 
 	<?php 
-    for ($i=1; $i<=9; $i++) 
-    {
-    	echo "<input type='button' value='$i'>";
-    	if ($i%3 ==0) {
+     for ($i=9; $i>=1; $i--) 
+     {    
+        echo "<input id='numeros' type='button' value='$i' onClick=entrada.value+='$i'>";
+    	if ($i% 3 == 1) 
+         {
     		echo "<br/>";
-    	}
-    }
-	?>
+    	 }
+     }
+    ?>
 	    <table>
              <tr>
-             	<td><input id="botao" type="button" value="0"></td>
-             	<td><input id="botao" type="button" value="."></td>
-             	<td><input id="botao" type="button" value="="></td>
-             	<td><input id="botao" type="button" value="+"></td>
+             	 <td><input id="botao" type="button" value="0" onClick="entrada.value+='0'"></td>
+             	 <td><input id="botao" type="button" value="." onClick="entrada.value+='.'"></td>
+             	 <td><input id="botao" type="button" value="=" onClick="entrada.value=eval(entrada.value)"></td>
+             	 <td><input id="botao" type="button" value="+" onClick="entrada.value+='+'"></td>
              </tr>
 		</table>
+    </form>
+</div>    
 </body>
 </html>
